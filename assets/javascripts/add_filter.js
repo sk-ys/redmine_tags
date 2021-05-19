3c4ea291a622ff68c6d3b0ce04857ce9704bd848
sk-ys <963792+sk-ys@users.noreply.github.com>
2021-05-20 05:25:00 +0900

    Fixed when filter selector does not exist

--- assets/javascripts/add_filter.js
+++ assets/javascripts/add_filter.js
@@ -1,4 +1,8 @@
 $(function () {
+    if ($('#filters').length !== 1) {
+        return;
+    }
+
     var update_option = function () {
         if ($('#operators_tags').length && !$('#operators_tags option[value="and"]').length) {
             var option_equals = $('#operators_tags option[value="="]');
@@ -8,19 +12,21 @@ $(function () {
     }
 
     var observer_rsize_filter =
-            new MutationObserver(function (mutations) {
-                update_option();
-            });
+        new MutationObserver(function (mutations) {
+            update_option();
+        });
     observer_rsize_filter.observe(
         $('#filters')[0], {
-            childList : true,
+            childList: true,
             subtree: true
         });
 
     // initial invoke
     update_option();
 
-    if (location.search.indexOf(encodeURI('op[tags]=and'))>0) {
-        setTimeout(function () {$('#operators_tags').val('and');});
+    if (location.search.indexOf(encodeURI('op[tags]=and')) > 0) {
+        setTimeout(function () {
+            $('#operators_tags').val('and');
+        });
     }
 });
